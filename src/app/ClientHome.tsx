adf0aca4ec66df64d9d05b4874857b9e52f4bf07
Eman_Ayman <emanaymanoffical@gmail.com>
2025-07-16 09:26:32 +0300

    updata clientHome component

--- src/app/ClientHome.tsx
+++ src/app/ClientHome.tsx
@@ -5,8 +5,6 @@ import { Box } from '@mui/material';
 import PostCreation from './_Components/PostCreation/PostCreation';
 import Post from './_Components/Post/Post';
 import { PostType } from './_interfaces/posts.types';
-import { Grid } from '@mui/material';
-
 
 type Props = {
   posts: PostType[];
@@ -15,26 +13,31 @@ type Props = {
 export default function ClientHome({ posts }: Props) {
   return (
     <Box sx={{ bgcolor: '#f0f2f5', minHeight: '100vh', py: 4 }}>
-      <Grid container justifyContent="center">
-        <Grid
-          item={true}
-          xs={12}
-          sm={8}
-          md={6}
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
           sx={{
+            width: {
+              xs: '100%', 
+              sm: '80%',  
+              md: '60%',  
+            },
             display: 'flex',
             flexDirection: 'column',
             gap: 3,
           }}
         >
-          <PostCreation onPostCreated={function (): void {
-            throw new Error('Function not implemented.');
-          } } />
+          <PostCreation onPostCreated={() => {}} />
           {posts.map((post) => (
             <Post key={post.id} postDetails={post} />
           ))}
-        </Grid>
-      </Grid>
+        </Box>
+      </Box>
     </Box>
   );
 }
